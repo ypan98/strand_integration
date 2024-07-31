b3c010b72efdc09713383efe2b574a5d92a4c6a8
Yimin Pan <yimin.pan.123@gmail.com>
2024-07-31 17:37:14 +0200

    minor

--- run_full_pipeline.sh
+++ run_full_pipeline.sh
@@ -3,22 +3,19 @@
 original_data_folder=$1
 folder_name=$(basename "$original_data_folder")
 
-echo "Converting: $original_data_folder..."
+echo "######################## Converting data "########################
 python convert_data.py --input $original_data_folder --skip_orientation
-echo "Data conversion complete."
 
-echo "**************** Running LPMVS pipeline ****************"
-echo "Estimating depth maps and 3d orientations..."
+echo "######################## Running LPMVS pipeline "########################
+echo "############ Estimating depth maps and 3d orientations ############"
 python run_lpmvs.py data/$folder_name -o result/lpmvs/$folder_name 
-echo "Filtering output..."
+echo "############ Filtering output ############"
 python run_line_filtering.py result/lpmvs/$folder_name result/merged_ply/lpmvs/$folder_name.ply
-echo "**************** LPMVS pipeline complete ****************"
 
-echo "**************** Running Strand Integration pipeline ****************"
-echo "Estimating depth maps and 3d orientations..."
+echo "######################## Running Strand Integration pipeline ########################"
+echo "############ Estimating consistency map ############"
 python run_consistency_map.py result/lpmvs/$folder_name -o result/consistency/$folder_name
-echo "Filtering output..."
+echo "############ Executing strand integration ############"
 python run_strand_integration.py result/lpmvs/$folder_name --consistency result/consistency/$folder_name -o result/si/$folder_name
-echo "Filtering output..."
+echo "############ Filtering output ############"
 python run_line_filtering.py result/si/$folder_name result/merged_ply/si/$folder_name.ply
-echo "**************** Strand Integration pipeline complete ****************"
